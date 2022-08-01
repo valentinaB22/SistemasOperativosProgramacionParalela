c312e59012aa02bce81741ba50affc8e7a4332b2
valentinaB22 <74430436+valentinaB22@users.noreply.github.com>
2022-08-01 15:38:16 -0300

    Add files via upload

---
+++ MergesortParalelo.c
@@ -0,0 +1,86 @@
+#include <omp.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#define dim 1000000 
+struct timeval t1, t2;
+
+int min(int x, int y) { 
+    return (x<y)? x :y; 
+}
+
+void merge(int* arr, int l, int m, int r){
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+    int* L=(int*)malloc(sizeof(int) * n1);
+    int* R=(int*)malloc(sizeof(int) * n2);
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2){
+        if (L[i] <= R[j]){
+            arr[k] = L[i];
+            i++;
+        }
+        else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1){
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2){
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int* arr, int n){
+   int curr_size; 
+   int left_start; 
+   for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size){
+       #pragma omp parallel for
+       for (left_start=0; left_start<n-1; left_start += 2*curr_size){
+           int mid = min(left_start + curr_size - 1, n-1);
+           int right_end = min(left_start + 2*curr_size - 1, n-1);
+           merge(arr, left_start, mid, right_end);
+       }
+   }
+}
+
+//Funcion para chequear si el arreglo está ordenado
+void check(int* A, int n){
+    for (int i = 0; i < n - 1; i++){
+        if (A[i] > A[i + 1]){
+            printf("Arreglo desordenado \n");
+            return;
+        }
+    }
+    printf("Arreglo ordenado \n");
+}
+
+int main(){
+    int* arr = (int*)malloc(sizeof(int) * dim);
+    srand(time(NULL));
+    for(int j=0;j<dim;j++){
+        arr[j]=rand() % dim +1;
+    }
+    check(arr, dim);
+    omp_set_num_threads(4); //cant de threads
+    gettimeofday(&t1, 0);
+    mergeSort(arr, dim);
+    gettimeofday(&t2, 0);
+    check(arr, dim);double time = (1000000.0*(t2.tv_sec-t1.tv_sec) + t2.tv_usec-t1.tv_usec)/1000.0;
+    printf("MERGESORT PARALELO ---- Tiempo de ejecucion: %3.3f ms \n", time/1000.0);
+    return 0;
+}
---
+++ Quicksort.c
@@ -0,0 +1,68 @@
+#include <sys/time.h>
+#include <omp.h>
+#include<stdio.h>
+#include <stdlib.h>
+#define dim 1000000 
+struct timeval t1, t2;
+
+int partition(int * a, int ini, int fin){
+
+    int* a1 = (int*)malloc(sizeof(int) * (fin-ini));
+    int* a2 = (int*)malloc(sizeof(int) * (fin-ini));
+    int pivot = a[fin]; 
+    int a1_n = 0;
+    int a2_n = 0;
+
+    for(int i = ini; i < fin; i++){
+        if(a[i] < a[fin]){
+            a1[a1_n++] = a[i];
+        }else{
+            a2[a2_n++] = a[i];
+        }   
+    }   
+    for(int i = 0; i < a1_n; i++){
+        a[ini + i] = a1[i];
+    }   
+    a[ini + a1_n] = pivot;
+    for(int j = 0; j < a2_n; j++){
+        a[ini + a1_n + j + 1] = a2[j];
+    }   
+    return ini + a1_n;
+}
+
+void quicksort(int * a, int ini, int fin){
+    int div;
+    if(ini < fin){ 
+        div = partition(a, ini, fin); 
+        quicksort(a, ini, div - 1); 
+        quicksort(a, div + 1, fin); 
+    }
+}
+
+//Funcion para chequear si el arreglo está ordenado
+void check(int* A, int n){
+    for (int i = 0; i < n - 1; i++){
+        if (A[i] > A[i + 1]) {
+            printf("Arreglo desordenado \n");
+            return;
+        }
+    }
+    printf("Arreglo ordenado \n");
+}
+
+int main()
+{
+    int* arr = (int*)malloc(sizeof(int) * dim);
+    srand(time(NULL));
+    for(int j=0;j<dim;j++){
+        arr[j]=rand() % dim +1;
+    }
+    check(arr, dim);
+    gettimeofday(&t1, 0);
+    quicksort(arr,0, dim-1);
+    gettimeofday(&t2, 0);
+    check(arr, dim);
+    double time = (1000000.0*(t2.tv_sec-t1.tv_sec) + t2.tv_usec-t1.tv_usec)/1000.0;
+    printf("QUICKSORT ---- Tiempo de ejecucion: %3.3f ms \n", time/1000.0);
+    return 0;
+}
